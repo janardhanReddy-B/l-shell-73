2c01e613df2e69f8892951ccc299e6bb478164e0
JanardhanReddy <bommireddy246@gmail.com>
2023-06-28 12:05:56 +0100

    FRONTEND SCRIPT

---
+++ 03-redirect.sh
@@ -0,0 +1,6 @@
+# < ---> this symbol is input to file
+# > or 1> ---> this symbol is output to file
+# >> ---> appended to file
+# 2> ---> this symbol is error output to file
+# both output and error we can use "&>"
+# if don't want output we can use this special dir ">/dev/null" --> this will Nullify the output

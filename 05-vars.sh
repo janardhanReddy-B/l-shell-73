24218654e4d7eff78222af385fa9d9403a66182a
JanardhanReddy <bommireddy246@gmail.com>
2023-06-28 23:34:17 +0100

    errors

---
+++ 05-vars.sh
@@ -0,0 +1,17 @@
+#in shell we don't have data type, simply we can declare value
+#varaiables names should be numbers and alphabets and underscore
+#if values have any special characters we need to double quotas
+
+#a=10
+#name-john
+#pass-1234klnm
+
+
+color="\e[33m"
+
+a=10
+
+echo value of a is $a
+
+#another way accessing variable is ${a}
+echo value of a is ${a}
